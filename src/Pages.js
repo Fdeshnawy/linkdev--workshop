49821efc2728ee6338370452f4c5dc245d38039a
Farah <f.deshnawy@aucegypt.edu>
2022-12-04 20:16:02 +0200

    final steps

--- src/Pages.js
+++ src/Pages.js
@@ -3,14 +3,19 @@ import App from "./App";
 import Newsitem from "./components/News/Newsitem";
 import News from "./components/News/News";
 
+
 function Pages() {
   return (
     <div>
+    
       <BrowserRouter>
+   
         <Routes>
+          
+        <Route path="/" element={<App />} />
           <Route path="/news" element={<News />} />
           <Route path="/newsItem/:id" element={<Newsitem />} />
-          <Route path="/" element={<App />} />
+        
         </Routes>
       </BrowserRouter>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { StrictMode } from "react";
@@ -13,6 +12,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+  
     <Pages />
   </StrictMode>
 );
